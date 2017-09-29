d8f1475ea55c04a35de7cd74019f1931ea87c672
mringel <matthew.r.ringel@gmail.com>
2017-09-29 11:41:21 -0700

    added body-parser middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.set('view engine', 'pug');
 
@@ -12,6 +14,15 @@ app.get('/cards', function(req, res) {
   res.locals.prompt = "Who is buried in Grant's tomb?"
   res.locals.hint = "Think about whose tomb it is."
   res.render('card');
+});
+
+app.get('/hello', function(req, res) {
+  res.render('hello');
+});
+
+app.post('/hello', function(req, res) {
+  console.dir(req.body);
+  res.render('hello');
 });
 
 app.listen(3000, () => {
