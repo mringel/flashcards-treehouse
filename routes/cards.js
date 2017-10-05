714ab231ec3875e454b89962c03c5fd52a86f26f
mringel <matthew.r.ringel@gmail.com>
2017-10-05 15:33:49 -0700

    added card routes and redirects, cleaned up templating

--- routes/cards.js
+++ routes/cards.js
@@ -3,12 +3,36 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor( Math.random() * numberOfCards);
+  res.redirect(`/cards/${flashcardId}?side=question`);
+});
+
 router.get('/:id', function(req, res) {
-  res.locals.prompt = cards[req.params.id].question;
-  res.locals.hint = cards[req.params.id].hint;
-  res.render('card');
-  // or it can be formatted like
-  // res.render('card', {prompt: 'Who is buried..?'});
+  const { side } = req.query;
+  const { id } =  req.params;
+
+  if ( !side ) {
+    res.redirect(`/cards/${id}?side=question`);
+
+  }
+  const name = req.cookies.username;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+
+  const templateData = { id, text, name };
+
+  if (side === 'question') {
+    templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
+  }
+  console.dir(templateData);
+  res.render('card', templateData);
 });
 
 module.exports = router;
